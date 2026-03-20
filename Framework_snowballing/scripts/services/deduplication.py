d1412b6f2a3b1543f0b5ea5157630c533d4765f5
Jailma Januario <jailmajanuario@gmail.com>
2026-03-19 22:41:11 -0300

    update application

---
+++ Framework_snowballing/scripts/services/deduplication.py
@@ -0,0 +1,56 @@
+from services.normalize import normalize_doi, generate_paper_id
+from services.matching import build_dedup_key, score_citation, merge_prefer_filled
+
+
+def parse_single_citation(c):
+    title = c.get("title", "-")
+    year = c.get("year", "-")
+    venue = c.get("venue", "-")
+
+    raw_doi = None
+    if isinstance(c.get("externalIds"), dict):
+        raw_doi = c["externalIds"].get("DOI")
+    if not raw_doi:
+        raw_doi = c.get("doi")
+
+    normalized_doi = normalize_doi(raw_doi)
+    doi = normalized_doi or "-"
+
+    abstract = c.get("abstract", "-")
+    authors = c.get("authors", [])
+    autores = [{"name": a.get("name", "-")} for a in authors]
+    total_citations_received = c.get("citationCount", 0)
+
+    return {
+        "paperId": generate_paper_id(title),
+        "title": title,
+        "authors": autores,
+        "year": year,
+        "venue": venue,
+        "doi": doi,
+        "abstract": abstract,
+        "citations_count": total_citations_received,
+    }
+
+
+def deduplicate_citations(raw_citations):
+    best_by_key = {}
+
+    for c in raw_citations:
+        citation_obj = parse_single_citation(c)
+        dedup_key = build_dedup_key(
+            citation_obj.get("title"),
+            citation_obj.get("doi")
+        )
+
+        if dedup_key not in best_by_key:
+            best_by_key[dedup_key] = citation_obj
+            continue
+
+        current_best = best_by_key[dedup_key]
+        if score_citation(citation_obj) > score_citation(current_best):
+            best_by_key[dedup_key] = citation_obj
+        else:
+            best_by_key[dedup_key] = merge_prefer_filled(current_best, citation_obj)
+
+    return list(best_by_key.values())
---
+++ Framework_snowballing/scripts/services/normalize.py
@@ -0,0 +1,64 @@
+import re
+import hashlib
+from difflib import SequenceMatcher
+
+
+def slugify(text):
+    text = re.sub(r"[^\w\s-]", "", str(text)).strip().lower()
+    return re.sub(r"[-\s]+", "-", text)[:50]
+
+
+def generate_paper_id(title):
+    if not title:
+        return "id-desconhecido"
+    slug = slugify(title)
+    return slug or hashlib.md5(str(title).encode()).hexdigest()[:10]
+
+
+def normalize_doi(doi):
+    if not doi:
+        return None
+
+    doi = str(doi).strip().lower()
+
+    if doi in {"", "-", "none", "null"}:
+        return None
+
+    doi = doi.replace("https://doi.org/", "")
+    doi = doi.replace("http://doi.org/", "")
+    doi = doi.replace("doi.org/", "")
+    doi = doi.replace("doi:", "")
+    doi = doi.strip()
+
+    if doi in {"", "-", "none", "null"}:
+        return None
+
+    return doi
+
+
+def normalize_title(title):
+    if not title:
+        return ""
+
+    title = str(title).strip().lower()
+    title = re.sub(r"[^\w\s]", "", title)
+    title = re.sub(r"\s+", " ", title)
+    return title
+
+
+def fuzzy_match(a, b):
+    return SequenceMatcher(None, normalize_title(a), normalize_title(b)).ratio()
+
+
+def token_overlap_score(a, b):
+    a_tokens = set(normalize_title(a).split())
+    b_tokens = set(normalize_title(b).split())
+
+    if not a_tokens:
+        return 0
+
+    return len(a_tokens & b_tokens) / len(a_tokens)
+
+
+def is_missing(value):
+    return value in (None, "", "-", "null", "None")
